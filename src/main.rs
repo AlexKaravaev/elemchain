34e0e037c0d17a9b1b311f4f4a5a1dcabd23d3b6
AlexKaravaev <alexkaravev@gmail.com>
2022-01-08 03:44:53 +0300

    Add main cli programm

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
 mod block;
-mod transaction;
 mod blockchain;
-mod node; 
+mod node;
+mod transaction;
+
+use blockchain::Blockchain;
+use dialoguer::{theme::ColorfulTheme, Select};
+
+pub fn handle_print_chain(chain: &Blockchain){
+    println!("{}", chain);
+}
 
 fn main() {
-    println!("Hello, world!");
+    let selections = &[
+        "Create block",
+        "View local blockchain",
+        "Generate transaction",
+    ];
+
+    let mut blockchain = Blockchain::new(0, 5, 256);
+    let mut node = node::Node { blockchain };
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Pick option")
+            .default(0)
+            .items(&selections[..])
+            .interact()
+            .unwrap();
+        if selection == 0{
+            let suc = node.blockchain.add_block(vec![]);
+        }
+        if selection == 1{
+            handle_print_chain(&node.blockchain);
+        }
+    }
 }
--- src/node.rs
+++ src/node.rs
@@ -1,11 +1,11 @@
 use crate::blockchain::Blockchain;
 
-struct Node {
-    blockchain: Blockchain,
+pub struct Node {
+    pub blockchain: Blockchain,
 }
 
 impl Node {
-    fn resolve_chain_conflict(&mut self, other: Blockchain) {
+    fn resolve_chain_conflict(&mut self, other: &Blockchain) {
         let own_valid = self.blockchain.is_valid();
         let other_valid = other.is_valid();
         let mut correct_chain;
@@ -31,6 +31,29 @@ impl Node {
 
 #[cfg(test)]
 mod tests {
+    use crate::block::tests::generate_blocks;
+    use crate::blockchain::tests::generate_blockchain;
 	use crate::{blockchain::Blockchain, node::Node, block::Block};
 
+    #[test]
+    fn test_conflict(){
+        let chain = generate_blockchain();
+
+        assert!(chain.is_valid());
+
+        let mut invalid_chain = generate_blockchain();
+
+        invalid_chain.chain.append(&mut generate_blocks());
+
+        assert!(!invalid_chain.is_valid());
+
+        let mut node = Node{
+            blockchain: invalid_chain,
+        };
+
+        node.resolve_chain_conflict(&chain);
+
+        assert!(node.blockchain == chain);
+
+    }
 }
